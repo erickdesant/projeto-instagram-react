53550ca5a4a8d0869d667e68b88c139353b0c05f
Erick Santana <ericksantana2009@gmail.com>
2024-08-22 01:49:32 -0300

    stories component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import Navbar from "./Navbar";
+import Body from "./Body";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+    <>
+        <Navbar/>
+        <Body/>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+      /*  App
+        Body
+        SideBar
+        NavBar
+        User
+        Stories (onde cada item será um componente diferente, o Story)
+        Posts (onde cada item será um componente diferente, o Post)
+        Suggestions (onde cada item será um componente diferente, o Suggestion)
+
+        <Body>
+            <Stories/>
+            <Post/>
+            <SideBar>
+                <User/>
+                <Suggestions>
+                    <Suggestion/>
+                </Suggestions>
+            </SideBar>
+        </Body>
+
+
+        */
---
+++ src/Stories.js
@@ -0,0 +1,52 @@
+export default function Stories(){
+    const stories = [
+        {
+            nome:"meowed",
+            imagem:"assets/img/meowed.svg"
+        },
+        {
+            nome:"barked",
+            imagem:"assets/img/barked.svg"
+        },
+        {
+            nome:"nathanwpylestrangeplanet",
+            imagem:"assets/img/nathanwpylestrangeplanet.svg"
+        },
+        {
+            nome:"wawawicomics",
+            imagem:"assets/img/wawawicomics.svg"
+        },
+        {
+            nome:"respondeai",
+            imagem:"assets/img/respondeai.svg"
+        },
+        {
+            nome:"filomoderna",
+            imagem:"assets/img/filomoderna.svg"
+        },
+        ]
+
+
+
+    return (
+        <div className="stories">
+            {stories.map((story) => <Story nome = {story.nome} imagem = {story.imagem}/>)}
+            <div className="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+        </div>
+    )
+}
+
+function Story({imagem,nome}){
+    return (
+        <div className="story">
+            <div className="imagem">
+                <img src={imagem} alt={nome}/>
+            </div>
+            <div className="usuario">
+                {nome}
+            </div>
+        </div>
+    )
+}
